b0c188514d5d73422c3294d9d11b2464a8289e56
Manuel Yang <myang@nativo.com>
2018-08-16 00:48:30 -0700

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# Lotecom
-Low tech communication board application
-
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import './print.css';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
